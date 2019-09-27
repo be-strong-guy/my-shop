3ea782cd82c8c9669afa77b23dad8e5037b72b2b
zrj1397632124 <1397632124@qq.com>
2019-09-27 08:43:27 +0800

    Merge branch 'master' of E:\IDEAworkspace\test with conflicts.

---
+++ my-shop/src/main/java/com/zrj/my/shop/commons/constant/ConstantUtils.java
@@ -0,0 +1,10 @@
+package com.zrj.my.shop.commons.constant;
+
+/**
+ * @program: ConstantUtils
+ * @description:
+ * @author: zrj
+ * @create: 2019-09-19 14:46
+ **/
+public class ConstantUtils {
+}
---
+++ my-shop/src/main/java/com/zrj/my/shop/web/interceptor/LoginInterceptor.java
@@ -0,0 +1,10 @@
+package com.zrj.my.shop.web.interceptor;
+
+/**
+ * @program: LoginInterceptor
+ * @description:
+ * @author: zrj
+ * @create: 2019-09-19 14:48
+ **/
+public class LoginInterceptor {
+}
